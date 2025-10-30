c96006ac4a12552409824183b1f96ecf28fe9fd3
MCTM <mctm@odoo.com>
2025-10-30 17:33:33 -0600

    starter

---
+++ factorial.py
@@ -0,0 +1,14 @@
+class NumFactorial:
+    def __init__(self,number):
+        self.number=number
+
+    def factorial(self):
+        counter=self.number
+        fact=1
+        while counter > 0
+            fact *= counter
+            counter -= 1
+        return fact
+
+somefact=NumFactorial(4)
+print(somefact.factorial())
